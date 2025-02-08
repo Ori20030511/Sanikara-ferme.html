15273f6360ac1bd11c892b4876ecf754ae7a4fa6
Ori20030511 <bachirdrame408@gmail.com>
2025-02-08 17:44:22 +0000

    Add files via upload

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+// Afficher "Le serveur fonctionne correctement"
+echo "<h1>✅ Le serveur fonctionne correctement !</h1>";
+
+// Afficher la version de PHP
+echo "<p>Version PHP : " . phpversion() . "</p>";
+
+// Afficher les informations du serveur
+phpinfo();
+?>
